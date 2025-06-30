595ca59e160cda1b6f7948359920198e84d6cd9a
CodeVineDev <codevine2023@gmail.com>
2025-06-30 09:41:00 -0700

    more project integration

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // CURSOR LOGIC
 const circle = document.querySelector(".cursor-circle");
-let mouseX = 0, mouseY = 0, currentX = 0, currentY = 0;
+let mouseX = 0,
+  mouseY = 0,
+  currentX = 0,
+  currentY = 0;
 const cursorSpeed = 0.1;
 let animationActive = window.innerWidth >= 1024;
 
@@ -21,20 +24,21 @@ function animateCursor() {
   if (animationActive) {
     currentX += (mouseX - currentX) * cursorSpeed;
     currentY += (mouseY - currentY) * cursorSpeed;
-    circle.style.transform = `translate3d(${currentX - 7.5}px, ${currentY - 7.5}px, 0)`;
+    circle.style.transform = `translate3d(${currentX - 7.5}px, ${
+      currentY - 7.5
+    }px, 0)`;
   }
   requestAnimationFrame(animateCursor);
 }
 
 animateCursor();
 
-
 // SLIDER LOGIC
-const sliderTrack = document.getElementById('sliderTrack');
+const sliderTrack = document.getElementById("sliderTrack");
 const slides = Array.from(sliderTrack.children);
 
 // Duplicate slides once for seamless loop
-slides.forEach(slide => {
+slides.forEach((slide) => {
   const clone = slide.cloneNode(true);
   sliderTrack.appendChild(clone);
 });
@@ -52,7 +56,6 @@ function animateSlider() {
 
 animateSlider();
 
-
 // HAMBURGER LOGIC
 const hamburger = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobileMenu");
@@ -60,4 +63,13 @@ const mobileMenu = document.getElementById("mobileMenu");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("open");
   hamburger.classList.toggle("open");
+});
+
+const moreProject = document.getElementById("extend");
+const addProject = document.getElementById("extension");
+const icon = document.getElementById("icon");
+moreProject.addEventListener("click", () => {
+  // alert("SUCESS")
+  moreProject.innerHTML = "See Less" + icon.innerHTML  ;
+  addProject.classList.toggle("active");
 });
